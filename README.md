f6a95bd564310e1efad115881e643110892f2424
Victor Volovik <VictorVolovik@users.noreply.github.com>
2024-10-16 20:34:31 +0400

    Initial setup

---
+++ README.md
@@ -0,0 +1,23 @@
+# Build a Static Site Generator
+
+Project built foloowing Boot.Dev's [Build a Static Site Generator
+](https://www.boot.dev/courses/build-static-site-generator)
+
+
+## Start App
+
+```bash
+./main.sh
+```
+
+## Run Tests
+
+```bash
+./test.sh
+```
+
+## Run Python http server with generated content
+
+```bash
+python3 -m http.server -d public 8888
+```
---
+++ src/test_textnode.py
@@ -0,0 +1,25 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_repr(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        result = node.__repr__()
+        expected = "TextNode(This is a text node, bold text, None)"
+        self.assertEqual(result, expected)
+
+    def test_default_url(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        url = node.url
+        self.assertIsNone(url)
+
+
+if __name__ == "__main__":
+    unittest.main()
